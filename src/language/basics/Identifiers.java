afbbda665754fcf68353d6d32b0c0cc396c00047
Bharath Reddy <Bharath.nittech@gmail.com>
2022-11-08 21:56:04 +0530

    nov 8th 2022

---
+++ src/language/basics/Identifiers.java
@@ -0,0 +1,35 @@
+package language.basics;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		
+		String name = "Pramod";
+		
+//		1. Identifier should not be same as java reserved key word
+		//String boolean = "Pramod";
+		
+//		2. Identifier should not be same as java literals
+		//String null = "Pramod";
+		
+//		3. Identifier should not contain white space
+		//String my name = "Pramod";
+	
+//		4. Identifier should not contain special chars
+		//String my@Name = "Pramod";
+		
+//		5. Identifier should not start with number
+		//String 1empName = "Pramod";
+		
+//		6. Identifier should be unique
+		//String name = "XYZ";
+		
+//		7. Java is case sensitive programming language
+		int number = 10;
+		int Number = 20;
+		
+//		8. Java identifiers should be meaningful and try to maintain 3-15 chars 
+		String  onceUponATimeMyNameIsSoAndSoNow = "XYZ";
+	}
+
+}
