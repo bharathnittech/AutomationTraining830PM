086080b1c79d8c8b3ca93a59aa8be4b5120ae69e
Bharath Reddy <Bharath.nittech@gmail.com>
2022-11-18 21:27:15 +0530

    Nov 18 changes

---
+++ src/selenium/basics/FirstSeleniumClass.java
@@ -0,0 +1,14 @@
+package selenium.basics;
+
+public class FirstSeleniumClass {
+
+	public static void main(String[] args) {
+		
+		//1. Launch Browser (Browser=Chrome)
+		//2. Enter URL and Launch Application (URL =https://www.google.co.in/)
+		//3. Wait for 3 Sec
+		//4. Close the browser		
+
+	}
+
+}
