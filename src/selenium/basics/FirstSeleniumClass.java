ab6d31af6ae93a33c21d74cee2454ab0ba998888
Bharath Reddy <Bharath.nittech@gmail.com>
2022-11-18 21:39:29 +0530

    Nov 18 changes

--- src/selenium/basics/FirstSeleniumClass.java
+++ src/selenium/basics/FirstSeleniumClass.java
@@ -5,6 +5,8 @@ public class FirstSeleniumClass {
 	public static void main(String[] args) {
 		
 		//1. Launch Browser (Browser=Chrome)
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe"); //set the browser properties
+		
 		//2. Enter URL and Launch Application (URL =https://www.google.co.in/)
 		//3. Wait for 3 Sec
 		//4. Close the browser		
