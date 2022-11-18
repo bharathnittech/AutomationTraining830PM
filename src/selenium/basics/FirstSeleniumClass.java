dfcc620e1145c0d8494f6de19518e8f0b08f3045
Bharath Reddy <Bharath.nittech@gmail.com>
2022-11-18 21:45:55 +0530

    Nov 18 changes

--- src/selenium/basics/FirstSeleniumClass.java
+++ src/selenium/basics/FirstSeleniumClass.java
@@ -5,15 +5,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSeleniumClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		//1. Launch Browser (Browser=Chrome)
 		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe"); //set the browser properties
 		WebDriver driver = new ChromeDriver();
 			
 		//2. Enter URL and Launch Application (URL =https://www.google.co.in/)
+		driver.get("https://www.google.co.in/");
+		
 		//3. Wait for 3 Sec
-		//4. Close the browser		
+		Thread.sleep(3000);
+		
+		//4. Close the browser	
+		driver.close();//close current window
+		driver.quit();//close all the windows launched by selenium
 
 	}
 
